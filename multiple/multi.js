c6ff8fb42d0f7257e7ce945dafb2e398fa001580
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-02-12 14:27:04 +0530

    test1

--- multiple/multi.js
+++ multiple/multi.js
@@ -1,1 +1,150 @@
-console.log("welcome")
+// console.log("welcome")
+
+// Test: Web Development Fundamentals
+
+// Section A: Multiple Choice Questions (20 marks)
+
+
+// What does HTML stand for?
+// a) Hyper Text Markup Language
+// b) Hyperlinks and Text Markup Language
+// c) Home Tool Markup Language
+// d) Hyper Tool Markup Language
+
+// ANS :-A
+
+// Which of the following is NOT a valid CSS property?
+// a) font-family
+// b) background-color
+// c) text-style
+// d) margin
+
+// ANS:-C
+
+// Which event is triggered when a user clicks on an HTML element?
+// a) mouseover
+// b) click
+// c) keydown
+// d) hover
+
+// ANS:-B
+
+// In JavaScript, what does the === operator do?
+// a) Assigns a value to a variable
+// b) Checks if two values are equal in both value and type
+// c) Checks if two values are equal in value but not necessarily in type
+// d) None of the above
+
+// ANS :-B
+
+// What is the purpose of React props?
+// a) To store and manage component-specific data
+// b) To pass data from parent to child components
+// c) To handle user interactions
+// d) None of the above
+
+// ANS:-B
+
+
+// Implement a JavaScript function called calculateArea that takes two parameters (length and width) and returns the area of a rectangle.
+
+// function calculateArea (length,width){
+//     let area=length*width
+//     return area;
+// }
+// console.log(calculateArea(15,12))
+
+
+// Which of the following is used to declare an array in JavaScript?
+// a) {}
+// b) []
+// c) ()
+// d) <>
+
+// ANS:-B
+
+// What does the indexOf method return if the element is not found in the array?
+// a) -1
+// b) 0
+// c) undefined
+// d) null
+
+// ANS:- D
+
+// What does the charAt method return if the character is not found in the string?
+// a) -1
+// b) 0
+// c) undefined
+// d) null
+
+// ANS:- D
+
+// In JavaScript, objects are enclosed in:
+// a) {}
+// b) []
+// c) ()
+// d) <>
+
+// ANS:- A
+
+
+// What is the correct syntax for an if-else statement in JavaScript?
+// a) if condition { // code block } else { // code block }
+// b) if condition ( // code block ) else ( // code block )
+// c) if (condition) { // code block } else { // code block }
+// d) if (condition) [ // code block ] else [ // code block ]
+
+// ANS:-C
+
+
+// Write a JavaScript function called findMax that takes an array of numbers as input and returns the maximum number in the array.
+
+// let array =[5,,6,7,8,9,10,21,22]
+// let max=array[0]
+// for (let i=0;i<array.length;i++){
+//     if(array[i]>max){
+//         max=array[i]
+//     }
+// }
+// console.log(max)
+// Implement a function called reverseString that takes a string as input and returns the reversed string.
+// let string="hello"
+// let string1=string.split("")
+// let op=string1.reverse("")
+// let result=op.join("")
+// console.log(result)
+
+// let string ="hello"
+// let reverse =""
+// for (let i=string.length-1;i>=0;i--){
+//     reverse += string[i]
+// } 
+// console.log(reverse)
+
+// Create an object called person with properties name, age, and gender. Write a function called printPerson that takes the person object as input and prints their details.
+
+
+
+// Develop a JavaScript function called countVowels that takes a string as input and returns the number of vowels in the string.
+
+// let string = "hello world"
+// let count =0
+// let vowels =["a","e","i","o","u"]
+
+// for(result of string.toLowerCase()){
+//     if(vowels.includes(result)){
+//         count++
+//     }
+// }console.log(count)
+
+
+// Implement a function called reverseArray that takes an array as input and returns a new array with the elements reversed. (Example: Input: [1, 2, 3], Output: [3, 2, 1])
+// let array=[1,2,3]
+// let newarray=[]
+// for (let i=array.length-1;i>=0;i--){
+//    newarray.push(array[i])
+// }
+// console.log(newarray)
+
+
+// Write a function called isPalindrome that takes a string as input and returns true if the string is a palindrome, false otherwise.
